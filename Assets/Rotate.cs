ace8503454b7923094683ec43b3c790a1bee0b41
SilviaJIR <ylight2280@gmail.com>
2018-04-07 22:43:28 -0700

    initial commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+  public float speed = 1f;
+  private bool ctrlled = false;
+    
+  Ray ray;
+  RaycastHit hit;
+    
+
+  void OnMouseOver()
+    {
+        print ("Mouse is over GameObject.");
+    }
+
+    void OnMouseExit()
+    {
+        print ("Mouse is no longer on GameObject.");
+    }
+
+
+  void Update ()
+  {
+
+     ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+     if(Physics.Raycast(ray, out hit))
+     {
+        ctrlled = true;
+       // print ("ttt");
+     } else {
+        ctrlled = false;
+       // print ("fff");
+
+     }
+
+
+
+     //if (ctrlled) {
+     if (Input.GetKey(KeyCode.LeftArrow)) {
+       transform.Rotate(Vector3.up, speed, Space.World);
+     }
+
+     if (Input.GetKey(KeyCode.RightArrow)) {
+       transform.Rotate(Vector3.down, speed, Space.World);
+     }
+
+     if (Input.GetKey(KeyCode.UpArrow)) {
+       transform.Rotate(Vector3.right, speed, Space.World);
+     }
+
+     if (Input.GetKey(KeyCode.DownArrow)) {
+       transform.Rotate(Vector3.left, speed, Space.World);
+     }
+    // }
+
+  }
+
+}
+
+
