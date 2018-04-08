993d22425af1c347970b5a0f3888329ae69bb155
D1o0g9s <boodalina@gmail.com>
2018-04-08 12:54:00 -0700

    12:53

---
+++ Assets/Win.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Win : MonoBehaviour
+{
+    public GameObject outline;
+    private ShadowDetect.ShadowDetect shadowScript;
+
+
+    private void Start()
+    {
+        shadowScript = outline.GetComponent<ShadowDetect.ShadowDetect>();
+        Debug.Log("hi!");
+    }
+
+    public void CheckWin()
+    {
+    
+        if (shadowScript.IsOnShadow_state == ShadowDetect.ShadowState.ON)
+        {
+            Debug.Log("Too big!");
+        } else
+        {
+            Debug.Log("win!");
+            
+        }
+    }
+
+}
